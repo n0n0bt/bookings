3195d2ec481e974e932fe74c24f3081bc72c015f
Naumche <nj@skalar-design.de>
2024-03-14 15:42:14 +0100

    Working with form validation and responses

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/n0n0bt/bookings/internal/forms"
+
 // TemplateData is a shared struct used for rendering templates
 type TemplateData struct {
 	StringMap map[string]string
@@ -10,4 +12,5 @@ type TemplateData struct {
 	Flash     string
 	Warning   string
 	Error     string
+	Form *forms.Form
 }
